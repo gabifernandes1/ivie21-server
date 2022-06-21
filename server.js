9c1dbbeba27e3de00c5cd39f291d6b05ecafdeb4
gabi-fernandes <gabi.fernandes@ibm.com>
2022-06-20 21:43:52 -0300

    teste

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use(cors());
+
+const url =
+	'mongodb+srv://ivie21:VOC6w6r7u@ivie21.tkbqi.mongodb.net/?retryWrites=true&w=majority';
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+	console.log(`App UI available http://localhost:${port}`);
+});
+
+app.get('/getConvidados', (req, res, err) => {
+	try {
+		MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+			if (err) throw err;
+			var dbo = db.db('convidados');
+			dbo
+				.collection('convidados')
+				.find({})
+				.toArray(function (err, result) {
+					if (err) throw 'err';
+					res.header('Access-Control-Allow-Origin', '*');
+					res.send(result);
+				});
+		});
+	} catch (e) {
+		console.log(e);
+	}
+});
