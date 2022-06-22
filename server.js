4a598e28366f9c7c4e1f3744cf3a9a7c436cc85a
gabi-fernandes <gabi.fernandes@ibm.com>
2022-06-22 14:04:10 -0300

    ?

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var MongoClient = require('mongodb').MongoClient;
 require('dotenv/config');
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
 
 const port = process.env.PORT || 8000;
