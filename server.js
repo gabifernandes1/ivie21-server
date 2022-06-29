07d2318231b4e4ed6933ff66fb55f24f4645443b
gabi-fernandes <gabi.fernandes@ibm.com>
2022-06-28 22:18:57 -0300

    D

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ require('dotenv/config');
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
-app.use(cors({ origin: '*' }));
+app.use(
+	cors({
+		origin: [
+			'https://ivie21.com/',
+			'*',
+			'https://ivie21-server.herokuapp.com/',
+		],
+	})
+);
 
 const port = process.env.PORT || 8000;
 
