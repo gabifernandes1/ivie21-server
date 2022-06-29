eb20864bf7661c947f6dc73eeaed5c19f69125c7
gabi-fernandes <gabi.fernandes@ibm.com>
2022-06-28 22:41:24 -0300

    D

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ require('dotenv/config');
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
-app.use(cors({ origin: '*' }));
+app.use(
+	cors({
+		origin: ['https://ivie21-server.herokuapp.com/', 'https://ivie21.com/'],
+	})
+);
 const port = process.env.PORT || 8000;
 
 app.listen(process.env.PORT || 8000, () => {
@@ -29,7 +33,7 @@ app.get('/getConvidados', (req, res, err) => {
 				.find({})
 				.toArray(function (err, result) {
 					if (err) throw 'err';
-					res.header('Access-Control-Allow-Origin', '*');
+
 					res.send(result);
 					db.close();
 				});
@@ -47,7 +51,7 @@ app.post('/adicionar', (req, res, err) => {
 				.collection('convidados')
 				.insertOne(req.body, function (err, response) {
 					if (err) throw err;
-					res.header('Access-Control-Allow-Origin', '*');
+
 					res.send('1 document inserted');
 					res.end('Success');
 					db.close();
@@ -75,7 +79,7 @@ app.post('/confirmacao', (req, res, err) => {
 				.collection('convidados')
 				.updateOne(myquery, newvalues, function (err, response) {
 					if (err) throw err;
-					res.header('Access-Control-Allow-Origin', '*');
+
 					console.log(response, '/CONFIRMACAO');
 					res.send('1 document updated');
 					db.close();
@@ -99,7 +103,7 @@ app.post('/entrou', (req, res, err) => {
 				.collection('convidados')
 				.updateOne(query, newvalues, function (err, response) {
 					if (err) throw err;
-					res.header('Access-Control-Allow-Origin', '*');
+
 					console.log(response);
 					res.send('1 document updated');
 					db.close();
@@ -126,7 +130,7 @@ app.post('/check', (req, res, err) => {
 				.find(query)
 				.toArray(function (err, response) {
 					if (err) throw err;
-					res.header('Access-Control-Allow-Origin', '*');
+
 					console.log(response, '?');
 					res.send(response);
 					db.close();
