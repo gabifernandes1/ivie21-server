423da8f84c2c1c3509767cdea42ec2be5f8e3b8d
gabi-fernandes <gabi.fernandes@ibm.com>
2022-06-28 22:45:43 -0300

    D

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ require('dotenv/config');
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
-app.use(cors());
+app.use(
+	cors({
+		origin: '*',
+	})
+);
 const port = process.env.PORT || 8000;
 
 app.listen(process.env.PORT || 8000, () => {
